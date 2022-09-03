a5268a7e20d23b7db0ee0119f5d39fd7b494c904
michaelromanoo <20636897+michaelromanoo@users.noreply.github.com>
2022-09-03 16:00:15 +0800

    feat: remove prebuilt code & assets from create-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='f1dex__body'>
+			<div className='f1dex__body__search'>
+				<input />
+			</div>
+			<div className='f1dex__body__results'>
+				<p>result</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
