e7a90189bab6eee08516c42b61ecc94cdc23e944
michaelromanoo <20636897+michaelromanoo@users.noreply.github.com>
2022-09-03 16:31:34 +0800

    feat: convert css to scss & implement deskop and mobile styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 function App() {
 	return (
-		<div className='f1dex__body'>
-			<div className='f1dex__body__search'>
-				<input />
-			</div>
-			<div className='f1dex__body__results'>
-				<p>result</p>
+		<div className='f1dex'>
+			<h1>Formula 1</h1>
+			<div className='f1dex__body'>
+				<div className='f1dex__body__search'>
+					<input />
+				</div>
+				<div className='f1dex__body__results'>
+					<p>result</p>
+				</div>
 			</div>
 		</div>
 	);
