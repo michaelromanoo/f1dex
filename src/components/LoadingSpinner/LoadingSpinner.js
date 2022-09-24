4d26c35a2c075008e0b42c468aabdff7cbab2793
michaelromanoo <20636897+michaelromanoo@users.noreply.github.com>
2022-09-24 15:21:48 +0800

    feat: add spinner component & render it while loading

---
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import { ReactComponent as Spinner } from '../../assets/spinner.svg';
+
+const LoadingSpinner = () => {
+	return (
+		<div>
+			<Spinner />
+		</div>
+	);
+};
+
+export default LoadingSpinner;
