e0ab7f388d12824067e572f3fdc70709b9b72529
michaelromanoo <20636897+michaelromanoo@users.noreply.github.com>
2022-10-01 15:09:08 +0800

    feat: install and setup react query

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
 	</React.StrictMode>
 );
 
