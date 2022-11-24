10e64b00df35b177ad7b164a00503417a2e0a9b8
michaelromanoo <20636897+michaelromanoo@users.noreply.github.com>
2022-11-24 10:24:22 +0800

    feat: change api link from http to https

--- src/api/api.js
+++ src/api/api.js
@@ -3,27 +3,27 @@ import { useQuery } from '@tanstack/react-query';
 // fetch drivers list by year
 export const useFetchDrivers = (year) => {
 	return useQuery(['drivers', year], async () =>
-		(await fetch(`http://ergast.com/api/f1/${year}/drivers.json`)).json()
+		(await fetch(`https://ergast.com/api/f1/${year}/drivers.json`)).json()
 	);
 };
 
 // fetch driver info by driver id
 export const useFetchDriverInfo = (id) => {
 	return useQuery(['driverInfo', id], async () =>
-		(await fetch(`http://ergast.com/api/f1/drivers/${id}.json`)).json()
+		(await fetch(`https://ergast.com/api/f1/drivers/${id}.json`)).json()
 	);
 };
 
 // fetch all constructors in 2022
 export const useFetchConstructors = () => {
 	return useQuery(['constructors'], async () =>
-		(await fetch(`http://ergast.com/api/f1/2022/constructors.json`)).json()
+		(await fetch(`https://ergast.com/api/f1/2022/constructors.json`)).json()
 	);
 };
 
 // // fetch constructor info by id
 export const useFetchConstructorsInfo = (id) => {
 	return useQuery(['constructorsInfo', id], async () =>
-		(await fetch(`http://ergast.com/api/f1/constructors/${id}.json`)).json()
+		(await fetch(`https://ergast.com/api/f1/constructors/${id}.json`)).json()
 	);
 };
